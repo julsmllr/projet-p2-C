f3ee6d8df2653be15e0bebf08b36172d7addef08
Jules <julesmuller06@gmail.com>
2025-10-24 10:15:44 +0200

    Créer list Adjd

--- list_adjacence.c
+++ list_adjacence.c
@@ -1,18 +1,14 @@
-//
-// Created by jules on 24/10/2025.
-//
-
 #include "list_adjacence.h"
 
 liste_adjacence creerListeAdjacenceVide(int taille) {
     liste_adjacence adj;
     adj.taille = taille;
-    adj.tab = (liste )malloc(taille sizeof(liste));
+    adj.tab = (t_list**)malloc(adj.taille * sizeof(t_list*));
     if (!adj.tab) {
         perror("Erreur d’allocation mémoire pour la liste d’adjacence");
         exit(EXIT_FAILURE);
     }
     for (int i = 0; i < taille; i++)
-        adj.tab[i] = creerListeVide();
+        adj.tab[i] = createEmptyList();
     return adj;
 }
--- list_adjacence.h
+++ list_adjacence.h
@@ -2,10 +2,15 @@
 #define INC_2526_TI301_PJT_LIST_ADJACENCE_H
 
 #include "list.h"
+#include "list_adjacence.h"
 
 typedef struct {
     t_list** tab;
+    int taille;
 }liste_adjacence;
 
+liste_adjacence createEmptyTab(int size);
+
+void printTab(liste_adjacence);
 
 #endif //INC_2526_TI301_PJT_LIST_ADJACENCE_H
